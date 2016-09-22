7809d07999a31f2b72c5f6ca707ef99b1e8facfe
Tim Swast <tswast@gmail.com>
2016-09-21 21:29:58 -0700

    Add session to freeze the app.
    This is a poor-man's test. It'll make sure that there are no 404s or
    server errors. When I release Frozen-Flask 0.13 it will also ensure
    there are no redirects.
    
    I would still want to add some kind of "test" that at a minimum checked
    that there is content at some or all of the desired pages. Maybe some
    WebDriver tests or something, too?

--- nox.py
+++ nox.py
@@ -4,6 +4,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
+def session_freeze(session):
+    session.install('-r', 'requirements.txt')
+    session.run('python', 'freeze.py')
+
+
 def session_lint(session):
     session.install('flake8')
     session.run('flake8', '--exclude', '.nox,.cache,env', '.')
